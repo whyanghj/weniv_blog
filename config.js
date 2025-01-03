950e89fe91ebfe7f01d2ca3f3826fefde9eb929b
whyanghj <101460951+whyanghj@users.noreply.github.com>
2025-01-03 13:50:38 +0900

    저자 변경

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
+    username: "Yang",
     company: "weniv",
     position: "CEO",
     img: "img/user/profile-licat.png",
